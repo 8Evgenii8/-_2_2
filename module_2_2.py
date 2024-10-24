96b826a717601014da44c74e5a70e1ca3f296262
8Evgenii8 <jiganpro77@gmail.com>
2024-10-24 20:41:11 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,10 @@
+first = 42
+second = 69
+third = 42
+first = int(input("напишите число: "))
+if  first == second == third:
+    print(3)
+elif first == second or second == third or third == first:
+    print(2)
+else:
+    print(0)
